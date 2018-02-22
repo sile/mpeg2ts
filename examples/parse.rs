57431f05ca1375ca6bfb0df2c487c1a6ab8fe73d
Takeru Ohta <phjgt308@gmail.com>
2018-02-23 02:43:51 +0900

    Add es-{audio,video} option

--- examples/parse.rs
+++ examples/parse.rs
@@ -3,9 +3,11 @@ extern crate mpeg2ts;
 #[macro_use]
 extern crate trackable;
 
+use std::io::Write;
 use clap::{App, Arg};
 use mpeg2ts::pes::{PesPacketReader, ReadPesPacket};
 use mpeg2ts::ts::{ReadTsPacket, TsPacketReader};
+use trackable::error::Failure;
 
 fn main() {
     let matches = App::new("parse")
@@ -13,7 +15,7 @@ fn main() {
             Arg::with_name("OUTPUT_TYPE")
                 .long("output-type")
                 .takes_value(true)
-                .possible_values(&["ts", "pes", "es"])
+                .possible_values(&["ts", "pes", "es-audio", "es-video"])
                 .default_value("ts"),
         )
         .get_matches();
@@ -30,7 +32,32 @@ fn main() {
                 println!("{:?} {} bytes", packet.header, packet.data.len());
             }
         }
-        "es" => unimplemented!(),
+        "es-audio" => {
+            let mut reader = PesPacketReader::new(TsPacketReader::new(std::io::stdin()));
+            while let Some(packet) = track_try_unwrap!(reader.read_pes_packet()) {
+                if !packet.header.stream_id.is_audio() {
+                    continue;
+                }
+                track_try_unwrap!(
+                    std::io::stdout()
+                        .write_all(&packet.data)
+                        .map_err(Failure::from_error)
+                );
+            }
+        }
+        "es-video" => {
+            let mut reader = PesPacketReader::new(TsPacketReader::new(std::io::stdin()));
+            while let Some(packet) = track_try_unwrap!(reader.read_pes_packet()) {
+                if !packet.header.stream_id.is_video() {
+                    continue;
+                }
+                track_try_unwrap!(
+                    std::io::stdout()
+                        .write_all(&packet.data)
+                        .map_err(Failure::from_error)
+                );
+            }
+        }
         _ => unreachable!(),
     }
 }
